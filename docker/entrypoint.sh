248675dcf292ac7a8248e646509e17e138a496b1
Shady Gmira <shady.gmira@gmail.com>
2026-01-10 15:57:52 +0100

    Chore: Cleaned up docker + made python integration work in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,14 +5,8 @@ echo "[ENTRYPOINT] Starting LicthFeld-Studio container..."
 
 # Define variables
 PROJECT_DIR="/home/${USER}/projects/LichtFeld-Studio"
-TORCH_SRC="/home/${USER}/libtorch"
-TORCH_DEST="${PROJECT_DIR}/external/libtorch"
 
 
-echo "[ENTRYPOINT] Copying libtorch from ${TORCH_SRC} to ${TORCH_DEST}..."
 mkdir -p "${PROJECT_DIR}/external"
-rm -rf "${TORCH_DEST}"
-cp -r "${TORCH_SRC}" "${TORCH_DEST}"
-echo "[ENTRYPOINT] libtorch copied successfully."
 
 exec "${@:-bash}"
